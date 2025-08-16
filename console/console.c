051029850dd74f421f727eef43c5b910886fbfcf
Rina Tajvar <rina@anthropic.com>
2025-08-16 10:22:19 +0000

    Fix UDS bug: don't hardcode serverName to localhost
    When USE_UNIX_DOMAIN_SOCKETS is defined, the console client was incorrectly
    hardcoding serverName to 'localhost' regardless of the actual server name.
    This caused the server to misinterpret commands as console names, leading
    to 'automatic redirection disabled' errors.
    
    The bug affected all commands when UDS support was compiled in, even when
    using TCP connections with -M and -p flags.
    
    Fix: Use the actual server name regardless of UDS support.

--- console/console.c
+++ console/console.c
@@ -1626,11 +1626,7 @@ DoCmds(char *master, char *pports, int cmdi)
 	} else
 	    server = master;
 
-#if USE_UNIX_DOMAIN_SOCKETS
-	serverName = "localhost";
-#else
 	serverName = server;
-#endif
 
 	if (*ports == '\000') {
 #if USE_IPV6
